0f3dc77cc147d0e2fd2ad04f7df71c68eb4bb1be
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-16 15:18:47 +0530

    call later

--- 20240916/calllater.py
+++ 20240916/calllater.py
@@ -5,5 +5,9 @@ def calc(first:int, second:int)->int:
     quotient = first // second
     return sum, diff, product, quotient
 
-s,d,p,q = calc(20,6)
+s,d,p,q = calc(20,6) # 26 14 120 3
 print(s,d,p,q)
+
+t1 = calc(10,6) # (16,4,60,1)
+print(t1)
+print(type(t1))
