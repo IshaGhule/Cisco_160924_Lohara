39ebe178f0e5c5fb961d639f89a310f5f6174c00
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-18 10:47:10 +0530

    usecases of function

--- 20240916/function/calc.py
+++ 20240916/function/calc.py
@@ -31,16 +31,40 @@
 # print(find_sum(10,20,30,40,50)) #150
 
 
-def find_sum(first, second, **others):
-    s = first + second
-    if(len(others) > 0):
-        for key in others:
-            s += others[key]
-        #end for
-    #end if
-    #print(type(others))
-    return s
+# def find_sum(first, second, **others):
+#     s = first + second
+#     if(len(others) > 0):
+#         for key in others:
+#             s += others[key]
+#         #end for
+#     #end if
+#     #print(type(others))
+#     return s
 
-print(find_sum(first=10,second=20)) #30
-print(find_sum(first=10,second=20,third=30)) #60 
-print(find_sum(first=10,second=20,third=30,fourth=40,fifth=50))  #150
+# print(find_sum(first=10,second=20)) #30
+# print(find_sum(first=10,second=20,third=30)) #60 
+# print(find_sum(first=10,second=20,third=30,fourth=40,fifth=50))  #150
+
+# def fact(N):
+#     if N <= 1: #base/edge case
+#         return 1
+#     #endif
+#     return N * fact(N-1)
+
+# print(fact(5)) #120
+# print(fact(4)) #24
+
+import math
+def is_prime(N):
+    N_sqrt = int(math.sqrt(N))
+    for I in range(2,N_sqrt+1):
+        if N % I == 0:
+            return False
+        #endif
+    #endfor
+    return True
+
+print(is_prime(5))
+print(is_prime(51))
+print(is_prime(60))
+print(is_prime(61))
