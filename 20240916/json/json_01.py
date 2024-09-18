aecd5d17ec3d6f307f1801a8e2b059d665c66ab4
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-18 09:29:46 +0530

    json to objects

---
+++ 20240916/json/json_01.py
@@ -0,0 +1,20 @@
+#Convert JSON to objects and vice versa
+
+import json
+patients = [
+    {'id': 101, 'name': 'mahesh'},
+    {'id': 102, 'name': 'dravid'},
+    {'id': 103, 'name': 'isha'}
+]
+patients_str = json.dumps(patients)
+print(patients,patients_str)
+with open('patients_data.json','w') as patients_db:
+    json.dump(patients,patients_db)
+
+patients_list2 = json.loads(patients_str)
+print(patients_list2,type(patients_list2))
+
+with open('patients_data.json','r') as patients_db:
+    patients_list3 = json.load(patients_db)
+    print(patients_list3, type(patients_list3))
+
