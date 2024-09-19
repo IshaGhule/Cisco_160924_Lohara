baa30d2f71ab2fb1a94d795c41895763167b77a2
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-19 09:40:24 +0530

    falsk

--- 20240916/exception handling/divide.py
+++ 20240916/exception handling/divide.py
@@ -1,8 +1,7 @@
 def find_quotient(d,n):
-    if n== 0:
+    if n == 0:
         raise ZeroDivisionError()
     return d/n
-
 try:
     number = int(input("Enter a number: "))
     result = find_quotient(10, number)
