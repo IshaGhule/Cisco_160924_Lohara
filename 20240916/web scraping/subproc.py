c74806927226ba5331c44b835af55f147a287bad
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-19 15:52:38 +0530

    scrap

--- 20240916/web scraping/subproc.py
+++ 20240916/web scraping/subproc.py
@@ -1,5 +1,8 @@
 #win programming
 
 import subprocess
-result = subprocess.run(['python','-c','print(2**8)'],shell=True,capture_output=True,text=True)
+# result = subprocess.run(['python','-c','print(2**8)'],shell=True,capture_output=True,text=True)
+# print(result.stdout)
+
+result = subprocess.run(['dir'],shell=True,capture_output=True,text=True)
 print(result.stdout)
