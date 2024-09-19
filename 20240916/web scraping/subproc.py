189bfec5a9b9060506bde94b2f5f85b02f8d1a59
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-19 16:09:47 +0530

    os

--- 20240916/web scraping/subproc.py
+++ 20240916/web scraping/subproc.py
@@ -6,3 +6,15 @@ import subprocess
 
 result = subprocess.run(['dir'],shell=True,capture_output=True,text=True)
 print(result.stdout)
+
+#OS based commands:
+import os
+os.system('dir')
+os.popen('dir').read()
+cwd =os.getcwd()
+print("Current working directory: ", cwd)
+os.listdir()
+os.getcwd()
+#'C:\\Mywork\\Source\\Cisco_160924_Lohara'
+os.mkdir('new_dir')
+os.listdir()
