983f6398cc0cc2850721a5249c12737588d13191
IshaGhule <88487219+IshaGhule@users.noreply.github.com>
2024-09-20 10:47:27 +0530

    mproc

--- 20240916/mproc/proc1.py
+++ 20240916/mproc/proc1.py
@@ -1,4 +1,15 @@
 import multiprocessing
 import os
+import time
 def print_numbers():
-    pid = os.getpid
+    pid = os.getpid()
+    for i in range(5):
+        print(f'{i}@{pid}')
+        time.sleep(0.025)
+if __name__ == "__main__":
+    processes = []
+    for I in range(5):
+        process = multiprocessing.Process(target= print_numbers)
+        processes.append(process)
+        process.start()
+    print_numbers()
